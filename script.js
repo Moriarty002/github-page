d62ba750c96e98f1adbc57a07e15f8393a3dbe5f
Moriarty002 <43646155+Moriarty002@users.noreply.github.com>
2020-11-24 19:48:25 +0800

    link

---
+++ script.js
@@ -0,0 +1,6 @@
+var link = new Array();
+link[0] = "https://www.w3schools.com";
+link[1] = "https://www.youtube.com/";
+link[2] = "https://github.com/";
+var target_link = link[Math.random()*3];
+document.getElementById('need_link').href = target_link ;
