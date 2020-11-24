ac765338acb5240c8eb6ecf5b1da72c37355489a
Moriarty002 <43646155+Moriarty002@users.noreply.github.com>
2020-11-24 20:30:11 +0800

    complete

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ var link = new Array();
 link[0] = "https://www.w3schools.com";
 link[1] = "https://www.youtube.com/";
 link[2] = "https://github.com/";
-var target_link = link[Math.random()*3];
-document.getElementById("need_link").href = link[Math.random()*3] ;
+function choice(){
+    var target_link = link[Math.round(Math.random()*3)];
+    var t = document.getElementById("needlink");
+    t.href = target_link;
+}
