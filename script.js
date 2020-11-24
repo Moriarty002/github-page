555433266fc72f10870e4f88e2019d0cb9a650cb
Moriarty002 <43646155+Moriarty002@users.noreply.github.com>
2020-11-24 20:35:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ link[0] = "https://www.w3schools.com";
 link[1] = "https://www.youtube.com/";
 link[2] = "https://github.com/";
 function choice(){
-    var target_link = link[Math.round(Math.random()*3)];
+    var target_link = link[Math.floor(Math.random()*3)];
     var t = document.getElementById("needlink");
     t.href = target_link;
 }
