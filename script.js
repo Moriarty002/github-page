67814f3c9eee753ae53ad2ce3c31125f2618b064
Moriarty002 <43646155+Moriarty002@users.noreply.github.com>
2020-11-26 19:54:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 var link = new Array();
-link[0] = "https://www.w3schools.com";
-link[1] = "https://www.youtube.com/";
-link[2] = "https://github.com/";
+link[0] = "https://forms.gle/xiqzbbQT5ZULUDVC6";
+link[1] = "https://forms.gle/AU8BxGHPfUoztdd8A";
+link[2] = "https://forms.gle/hpCsMszSrzAYULF86";
+link[3] = "https://forms.gle/mogDeDHFwVPwuupL6";
+link[4] = "https://forms.gle/hfq5X9byQD1BDTi96";
+link[5] = "https://forms.gle/YrR2bBdrvLRmzaHQ7";
+link[6] = "https://forms.gle/3Ar7CdncNqBzRfy39";
+link[7] = "https://forms.gle/nC3VKJHRNy3MQfj67";
+link[8] = "https://forms.gle/RZvgAxskKPJWBHRt7";
+
+
 function choice(){
-    var target_link = link[Math.floor(Math.random()*3)];
+    var target_link = link[Math.floor(Math.random()*9)];
     var t = document.getElementById("needlink");
     t.href = target_link;
 }
